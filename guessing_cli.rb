b5c1d2aca95955b10e5ead0ae76447a6a29ea76e
Max Powell <max_f_powell@hotmail.co.uk>
2019-02-02 17:16:13 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
      puts "Guess a number between 1 and 6."
      guess = gets.chomp
      if guess == "exit"
-       puts "Goodbye."
+       puts "Goodbye!"
        break
      end
      puts "The computer guessed #{num}."
