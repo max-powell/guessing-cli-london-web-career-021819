42ba7df80b91e49964bfcad2f101f16ab4f48739
Max Powell <max_f_powell@hotmail.co.uk>
2019-02-02 17:11:08 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,5 +9,6 @@ def run_guessing_game
        puts "Goodbye!"
        break
      end
+     guess.to_i == num ? puts "You guessed the correct number!" : puts "The computer guessed #{num}."
    end
 end
